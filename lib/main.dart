f78c462f4c87f217a9ef58a2eceb66a44da226cb
AdityaSukses25 <adityawardana724@gmail.com>
2021-04-29 23:17:04 +0800

    project layouting

---
+++ lib/main.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()); 
+  
+
+  class MyApp extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        title: 'login',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primaryColor: Colors.black
+        ),
+        home: Login(),
+      );
+    }
+  }
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        color: Colors. white10,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget> [
+            Container(
+              width: 100,
+              height:100,
+              decoration:BoxDecoration(
+                color: Colors.black87,
+                shape: BoxShape. circle
+              ),
+
+              child: Center(
+                child:Icon(Icons.person,size:50, color: Colors.white,) ,
+
+                ),
+            ),
+
+            SizedBox(height: 20,),
+            
+              Text("SELAMAT DATANG ", style: TextStyle(fontSize: 20,color: Colors.black87),),
+
+              SizedBox(height: 20,),
+
+              TextFormField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black87) 
+
+                      ),
+                    prefixIcon:  Icon(Icons. person, size: 40,),
+                    hintText: "Masukan Username",
+                    hintStyle: TextStyle(color: Colors.black87),
+                    labelText: "Username",
+                    labelStyle: TextStyle(color: Colors.black87),
+                ),
+              ),
+
+              SizedBox(height: 20,),
+
+              TextFormField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black87) 
+
+                      ),
+                    prefixIcon:  Icon(Icons. looks, size: 40,),
+                    hintText: "Masukan Password",
+                    hintStyle: TextStyle(color: Colors.black87),
+                    labelText: "Passsword",
+                    labelStyle: TextStyle(color: Colors.black87),
+                ),
+              ),
+
+              SizedBox(height: 20,),
+
+              Card( 
+                color: Colors. black87,
+                elevation: 10,
+                child: Container(
+                  height: 40,
+                  child:Text ("LOGIN", style: TextStyle(fontSize: 15,color: Colors.white,),),
+                )
+              )
+          ],),
+      ),
+    );
+  }
+}
